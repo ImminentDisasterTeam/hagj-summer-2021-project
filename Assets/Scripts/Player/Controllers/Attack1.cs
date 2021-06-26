b1b03eec6362156ed7bf3ae27d6b90eebde8ba0c
Pirtness <edelal60@mail.ru>
2021-06-26 22:18:04 +0300

    add hit boxes for player  attacks

--- Assets/Scripts/Player/Controllers/Attack1.cs
+++ Assets/Scripts/Player/Controllers/Attack1.cs
@@ -6,7 +6,6 @@ public class Attack1 : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        Debug.Log("djfhgjdkls;fkjghfdkls;dkfjh");
-        animator.GetComponent<PlayerFighting>().ComboAttack1();
+        
     }
 }
--- Assets/Scripts/Player/PlayerFighting.cs
+++ Assets/Scripts/Player/PlayerFighting.cs
@@ -32,6 +32,10 @@ public class PlayerFighting : MonoBehaviour {
 
     [SerializeField] Vector2 combo1Offset;
     [SerializeField] float combo1Radius;
+    [SerializeField] Vector2 combo2Offset;
+    [SerializeField] float combo2Radius;
+    [SerializeField] Vector2 combo3Offset;
+    [SerializeField] float combo3Radius;
 
     //actions
     public Action<float> changeHealth;
@@ -103,7 +107,7 @@ public class PlayerFighting : MonoBehaviour {
             GetDamaged(10);
         }
     }
-
+    
     public void ComboAttack1() {
         Vector2 attackCentre = transform.TransformPoint(combo1Offset);
         var enemies = Physics2D.OverlapCircleAll(attackCentre, combo1Radius, enemyLayers);
@@ -112,11 +116,39 @@ public class PlayerFighting : MonoBehaviour {
         }
     }
 
+    public void ComboAttack2() {
+        Vector2 attackCentre = transform.TransformPoint(combo2Offset);
+        var enemies = Physics2D.OverlapCircleAll(attackCentre, combo2Radius, enemyLayers);
+        foreach (var enemy in enemies) {
+            enemy.GetComponent<EnemyController>().TakeDamage();
+        }
+    }
+    
+    public void ComboAttack3()
+    {
+        Vector2 attackCentre = transform.TransformPoint(combo3Offset);
+        Vector2 leftBottomPoint = attackCentre;
+        Vector2 rightTopPoint = attackCentre + new Vector2(combo3Radius, 2*combo3Radius);
+        var enemies = Physics2D.OverlapAreaAll(leftBottomPoint, rightTopPoint, enemyLayers);
+        foreach (var enemy in enemies) {
+            enemy.GetComponent<EnemyController>().TakeDamage();
+        }
+    }
+
     void OnDrawGizmosSelected()
     {
         // Draw a yellow sphere
-        Gizmos.color = Color.yellow;
-        Vector2 attackCentre = transform.TransformPoint(combo1Offset);
-        Gizmos.DrawSphere(attackCentre, combo1Radius);
+        // Gizmos.color = Color.yellow;
+        // Vector2 attackCentre = transform.TransformPoint(combo1Offset);
+        // Gizmos.DrawSphere(attackCentre, combo1Radius);
+        
+        // Gizmos.color = Color.green;
+        // Vector2 attackCentre = transform.TransformPoint(combo2Offset);
+        // Gizmos.DrawSphere(attackCentre, combo2Radius);
+        
+        Gizmos.color = Color.red;
+        Vector3 attackCentre = transform.TransformPoint(combo3Offset);
+        // Gizmos.DrawCube(attackCentre, combo3Radius);
+        Gizmos.DrawCube(attackCentre, new Vector3(combo3Radius, 2*combo3Radius, 0f));
     }
 }
